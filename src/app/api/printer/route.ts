2be720cae914ac0826cc7d41b71910eebb8ca0db
nafisfhkr <nafisfakhru@gmail.com>
2026-02-17 20:56:22 +0700

    fix:backend cloudinary upload gambar

--- src/app/api/printer/route.ts
+++ src/app/api/printer/route.ts
@@ -1,10 +1,10 @@
-import { NextRequest,NextResponse } from "next/server";
-import { PrinterSchema,ImageFileSchema } from "@/lib/validation";
-import { uploadImage } from "@/lib/cloudinary";
-import { createPrinter } from "@/services/printerService";
+import { NextRequest, NextResponse } from "next/server";
+import { PrinterSchema, ImageFileSchema } from "@/lib/validation"; 
+import { uploadImage } from "@/lib/cloudinary"; 
+import { createPrinter } from "@/services/printerService"; 
 
-export async function POST(req:NextRequest) {
- try{
+export async function POST(req: NextRequest) {
+  try {
     const formData = await req.formData();
     
     const file = formData.get("image") as File;
@@ -15,46 +15,58 @@ export async function POST(req:NextRequest) {
     const location = formData.get("location") as string;
     const whatsapp = formData.get("whatsapp") as string;
 
-
-    const validationFields = PrinterSchema.safeParse({
-        storeName,
-        description,
-        priceBlackWhite,
-        priceColor,
-        location,
-        whatsapp,
+  
+    const validatedFields = PrinterSchema.safeParse({
+      storeName,
+      description,
+      priceBlackWhite,
+      priceColor,
+      location,
+      whatsapp,
     });
 
-    if (!validationFields.success) {
-        return NextResponse.json(
-            {error: "Data tidak valid", details: validationFields.error.flatten()},
-            {status:400}
-        );
+    if (!validatedFields.success) {
+      return NextResponse.json(
+        { error: "Data tidak valid", details: validatedFields.error.flatten() },
+        { status: 400 }
+      );
     }
 
+    const validatedFile = ImageFileSchema.safeParse(file);
+
+    if (!validatedFile.success) {
+      return NextResponse.json(
+        { error: "File gambar bermasalah", details: validatedFile.error.flatten() },
+        { status: 400 }
+      );
+    }
+
+ 
+    
     const uploadResult: any = await uploadImage(file, "titipprint-dev");
-
-
+    
+  
     const newPrinterData = {
-        ...validationFields.data,
-        imageUrl: uploadResult.secure_url,
-        imageId : uploadResult.public_id,
-        ownerEmail: "user-test@gmail.com",
-        isOpen:true,
+      ...validatedFields.data, 
+      imageUrl: uploadResult.secure_url, 
+      imageId: uploadResult.public_id,   
+      ownerEmail: "user-test@gmail.com", 
+      isOpen: true,
     };
 
+   
     const savedPrinter = await createPrinter(newPrinterData);
 
     return NextResponse.json(
-        {message: "Berhasil upload!", data: savedPrinter},
-        {status:201}
+      { message: "Berhasil upload!", data: savedPrinter },
+      { status: 201 }
     );
 
-    } catch (error) {
-        console.error("API_Error:", error);
-        return NextResponse.json(
-            {error : "Terjadi Kesalahan server"},
-            {status: 500}
-        );
-    }
- }   
+  } catch (error) {
+    console.error("API Error:", error);
+    return NextResponse.json(
+      { error: "Terjadi kesalahan server" },
+      { status: 500 }
+    );
+  }
+}
